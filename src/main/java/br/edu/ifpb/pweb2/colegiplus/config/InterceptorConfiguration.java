73eda29bd914c7ac2ba4a202ec2820df27680f95
Laila Aquino <lailaaquino8@gmail.com>
2025-11-17 18:42:27 -0300

    add imports

--- src/main/java/br/edu/ifpb/pweb2/colegiplus/config/InterceptorConfiguration.java
+++ src/main/java/br/edu/ifpb/pweb2/colegiplus/config/InterceptorConfiguration.java
@@ -2,7 +2,8 @@ package br.edu.ifpb.pweb2.colegiplus.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.edu.ifpb.pweb2.colegiplus.interceptor.AuthInterceptor;
 
