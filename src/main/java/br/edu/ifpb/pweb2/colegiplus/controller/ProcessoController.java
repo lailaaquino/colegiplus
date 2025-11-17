a655ef48ef047b69ecfb4c703fb27130b62911c0
Luan Pimenta <pimentaluan2005@gmail.com>
2025-11-17 00:52:37 -0300

    add: controlador de processos para aluno, professor e coordenador

--- src/main/java/br/edu/ifpb/pweb2/colegiplus/controller/ProcessoController.java
+++ src/main/java/br/edu/ifpb/pweb2/colegiplus/controller/ProcessoController.java
@@ -1,46 +1,104 @@
 package br.edu.ifpb.pweb2.colegiplus.controller;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import br.edu.ifpb.pweb2.colegiplus.model.Assunto;
+import br.edu.ifpb.pweb2.colegiplus.model.Aluno;
+import br.edu.ifpb.pweb2.colegiplus.model.Colegiado;
 import br.edu.ifpb.pweb2.colegiplus.model.Processo;
+import br.edu.ifpb.pweb2.colegiplus.model.Professor;
+import br.edu.ifpb.pweb2.colegiplus.model.StatusProcesso;
 import br.edu.ifpb.pweb2.colegiplus.repository.AssuntoRepository;
+import br.edu.ifpb.pweb2.colegiplus.repository.ColegiadoRepository;
+import br.edu.ifpb.pweb2.colegiplus.repository.ProfessorRepository;
 import br.edu.ifpb.pweb2.colegiplus.service.ProcessoService;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/processos")
 public class ProcessoController {
 
     @Autowired
+    private ProcessoService processoService;
+
+    @Autowired
     private AssuntoRepository assuntoRepository;
 
     @Autowired
-    private ProcessoService processoService;
+    private ColegiadoRepository colegiadoRepository;
 
-    @RequestMapping("/form")
-    public ModelAndView getForm(ModelAndView modelAndView) {
-        modelAndView.setViewName("processos/form");
-        modelAndView.addObject("processo", new Processo());
-        return modelAndView;
+    @Autowired
+    private ProfessorRepository professorRepository;
+
+    @GetMapping
+    public ModelAndView listar(
+            @RequestParam(required = false) String status,
+            @RequestParam(required = false) Long assuntoId,
+            @RequestParam(required = false, defaultValue = "asc") String ordem,
+            HttpSession session) {
+
+        ModelAndView mv = new ModelAndView("processos/list");
+        String tipo = (String) session.getAttribute("tipoUsuario");
+        Object usuario = session.getAttribute("usuario");
+
+        List<Processo> processos = List.of();
+
+        if ("ALUNO".equals(tipo)) {
+            Aluno aluno = (Aluno) usuario;
+            processos = processoService.filtrarProcessosDoAluno(aluno, status, assuntoId, ordem);
+
+            mv.addObject("assuntos", assuntoRepository.findAll());
+            mv.addObject("statusSelecionado", status);
+            mv.addObject("assuntoSelecionado", assuntoId);
+            mv.addObject("ordemSelecionada", ordem);
+        }
+        else if ("PROFESSOR".equals(tipo)) {
+            Professor professor = (Professor) usuario;
+            processos = processoService.listarProcessosDoProfessor(professor);
+        }
+        else if ("COORDENADOR".equals(tipo)) {
+            Professor coord = (Professor) usuario;
+            processos = processoService.listarProcessosDoCoordenador(coord);
+
+            Colegiado colegiado = colegiadoRepository.findByCoordenador(coord);
+            List<Professor> membros = (colegiado != null)
+                    ? colegiado.getMembros()
+                    : professorRepository.findAll();
+
+            mv.addObject("membros", membros);
+            mv.addObject("statusSelecionado", status);
+        }
+
+        mv.addObject("processos", processos);
+        return mv;
     }
 
-    @ModelAttribute("assuntos")
-    public List<Assunto> getAssuntos() {
-        return assuntoRepository.findAll();
+
+    @GetMapping("/novo")
+    public ModelAndView formNovo(HttpSession session) {
+        String tipo = (String) session.getAttribute("tipoUsuario");
+        if (!"ALUNO".equals(tipo)) {
+            return new ModelAndView("redirect:/processos");
+        }
+
+        ModelAndView mv = new ModelAndView("processos/form");
+        mv.addObject("processo", new Processo());
+        mv.addObject("assuntos", assuntoRepository.findAll());
+        return mv;
     }
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public ModelAndView adicionaProcesso(Processo processo, ModelAndView modelAndView) {
-        processoService.save(processo);
-        modelAndView.setViewName("processos/list");
-        modelAndView.addObject("processos", processoService.findAll());
-        return modelAndView;
+    @PostMapping
+    public String salvar(Processo processo, HttpSession session) {
+        String tipo = (String) session.getAttribute("tipoUsuario");
+        if (!"ALUNO".equals(tipo)) {
+            return "redirect:/processos";
+        }
+        Aluno aluno = (Aluno) session.getAttribute("usuario");
+        processoService.saveForAluno(processo, aluno);
+        return "redirect:/processos";
     }
-
 }
