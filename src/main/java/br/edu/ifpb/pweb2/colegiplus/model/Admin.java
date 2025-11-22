8677de2bf32994c53d3400fd9d36bfde2c0283bf
Laila Aquino <lailaaquino8@gmail.com>
2025-11-22 13:13:52 -0300

    add classe admin

---
+++ src/main/java/br/edu/ifpb/pweb2/colegiplus/model/Admin.java
@@ -0,0 +1,33 @@
+package br.edu.ifpb.pweb2.colegiplus.model;
+
+import java.io.Serializable;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Admin implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String login;
+
+    @NotBlank
+    private String senha;
+}
