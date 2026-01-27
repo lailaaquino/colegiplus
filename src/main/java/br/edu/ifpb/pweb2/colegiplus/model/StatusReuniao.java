4d3214a7050e29d452becd3705e29cb955bdef63
Laila Aquino <lailaaquino8@gmail.com>
2026-01-27 15:17:50 -0300

    add novo status em reunião

--- src/main/java/br/edu/ifpb/pweb2/colegiplus/model/StatusReuniao.java
+++ src/main/java/br/edu/ifpb/pweb2/colegiplus/model/StatusReuniao.java
@@ -1,11 +1,12 @@
 package br.edu.ifpb.pweb2.colegiplus.model;
-import lombok.Getter;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public enum StatusReuniao {
     ENCERRADA(1), 
-    PROGRAMADA(2); 
+    PROGRAMADA(2), 
+    EM_JULGAMENTO(3);
     private final int codigo;
 }
