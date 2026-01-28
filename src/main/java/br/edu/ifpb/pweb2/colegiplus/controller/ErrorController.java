2cd2fa85089dd948a55e9dcb5601187f0b0cbbca
Leticia Lopes Lima <leticiazlopes@MacBook-Air-de-Leticia.local>
2026-01-28 08:01:06 -0300

    add error page

---
+++ src/main/java/br/edu/ifpb/pweb2/colegiplus/controller/ErrorController.java
@@ -0,0 +1,14 @@
+package br.edu.ifpb.pweb2.colegiplus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorController {
+
+    @GetMapping("/acesso-negado")
+    public String acessoNegado() {
+        return "auth/acesso-negado";
+    }
+}
+
