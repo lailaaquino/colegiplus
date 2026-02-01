7005c560490602be11a23a033164f735c76175f4
Your Name <leticiazlopes@MacBook-Air-de-Leticia.local>
2026-02-01 13:41:23 -0300

    Remove import desnecessario

--- src/main/java/br/edu/ifpb/pweb2/colegiplus/controller/AlunoController.java
+++ src/main/java/br/edu/ifpb/pweb2/colegiplus/controller/AlunoController.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
